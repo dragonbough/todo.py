5dc1e6bf42b165b45f8a84a220a157d2e161efd9
dragonbough <fiveteen81@gmail.com>
2022-11-26 22:39:19 +0000

    revamp... that still doesn't work

--- sorting.py
+++ sorting.py
@@ -2,39 +2,23 @@ import string
 import time
 
 list = []
-priorityList = []
-orderedPList = []
 orderedList = []
-originalList = []
 
-#For every item in the list (e.g apple, orange banana), it checks every single
-#letter of the item, and if that letter is in the alphabet and it is the first
-#letter of the word, it stores its priority in a variable which, if it is the 
-#latter letters of the word, the priority of the latter is decimalised and added onto the main priority (first letter's priority) and added to the priority list
-#The smallest number is then added to the orderedList and then removed from the priorityList. this order of the smallest number being removed from the priority list causes the 
-# orderedList to align in a consecutive order.
 def SortList(list):
+    itemPriority = 0
     for item in list:
-        for i in item:
-            if i in string.ascii_letters:
-                if item.index(i) == 0:
-                    firstPriority = string.ascii_letters.index(item[0]) + 1
-                elif item.index(i) > 0:
-                    latterPriority = (string.ascii_letters.index(item[item.index(i)])) / (10 ** (item.index(i)))
-                    priority = firstPriority + latterPriority
-        priorityList.append(priority)
-        originalList.append(priority)
-        
-    for item in list:
-        orderedPList.insert(0, max(priorityList))
-        priorityList.remove(max(priorityList))
-    for item in list:
-        orderedList.insert(orderedPList.index((originalList[(list.index(item))])), item)
-    return orderedList, orderedPList
-        
+        for letter in item:
+                nLetter = ord(letter)
+                if letter == item[0]:
+                    itemPriority += nLetter
+                elif letter != item[0]:
+                    itemPriority += (nLetter / (10 ** item.index(letter)))
+        print (itemPriority)
 
 listlength = int(input ("How many things are in your list?"))
 for i in range(listlength):
     list.append(input ("Add to your list"))
 print("Sorting list...")
 print(SortList(list))
+
+#I can't do this man ffs
