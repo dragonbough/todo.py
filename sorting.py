678a54a32a5231a7c22700cc0d38caef54485887
dragonbough <fiveteen81@gmail.com>
2023-01-11 18:53:20 +0000

    i fixed it by searching up the answer. onto the next one

--- sorting.py
+++ sorting.py
@@ -1,26 +1,16 @@
-import string
-import time
-
 list = []
-priorityList = []
+orderedList = []
 
 def SortList(list):
-    itemPriority = 0
-    for item in list:
-        for letter in item:
-                nLetter = ord(letter)
-                if letter == item[0]:
-                    itemPriority += nLetter
-                elif letter != item[0]:
-                    itemPriority += (nLetter / (10 ** item.index(letter) + len(str(nLetter))))
-        print(itemPriority)
-        priorityList.append(itemPriority)
-        print(priorityList)
-                
+    k = len(list)
+    for i in range(k):
+        for j in range (k - i - 1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
     return list
-listlength = int(input ("How many things are in your list?"))
-for i in range(listlength):
+
+
+listLength = input ("How many things are in your list?")
+for i in range(int(listLength)):
     list.append(input ("Add to your list"))
-print("Sorting list...")
 print(SortList(list))
-#Think
