703935d80df613f3681a00ac2f105e929d4bce02
Alex <99271006+Dragonbough@users.noreply.github.com>
2023-02-09 14:45:37 +0000

    75% done

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,51 +1,56 @@
-import keyboard
+row1 = [1, 2, 3]
+row2 = [4, 5, 6]
+row3 = [7, 8, 9]
 
-playerDigit = "X"
+def Display():
+    print (*row1)
+    print (*row2)
+    print (*row3)
 
-row1 = ["a", "b", "c"]
-row2 = ["d", "e", "f"]
-row3 = ["g", "h", "i"]
+def FindRow(location, symbol):
+    if location in row1:
+        row1[location - 1] = symbol
+    if location in row2:
+        row2[location - 4] = symbol
+    if location in row3:
+        row3[location - 7] = symbol
 
-uRow1 = ["_", "_", "_"]
-uRow2 = ["_", "_", "_"]
-uRow3 = ["_", "_", "_"]
-
-rows = [row1, row2, row3]
-
-def DrawMap():
-    print (*uRow1)
-    print (*uRow2)
-    print (*uRow3)
-    
-position = 0
-                
-def UpdateMap():
-    if position == "a":
-        uRow1[row1.index("a")] = playerDigit
-    elif position == "b":
-        uRow1[row1.index("b")] = playerDigit
-    elif position == "c":
-        uRow1[row1.index("c")] = playerDigit
-    elif position == "d":
-        uRow2[row2.index("d")] = playerDigit
-    elif position == "e":
-        uRow2[row2.index("e")] = playerDigit
-    elif position == "f":
-        uRow2[row2.index("f")] = playerDigit
-    elif position == "g":
-        uRow3[row3.index("g")] = playerDigit
-    elif position == "h":
-        uRow3[row3.index("h")] = playerDigit
-    elif position == "i":
-        uRow3[row3.index("i")] = playerDigit 
-    DrawMap()
-
-UpdateMap()
-
-while True:
-    if keyboard.is_pressed("a"):
-        position = "a"
-        UpdateMap()
-
+def CheckWin():
+    if any(i == ("X", "X", "X") for i in (row1, row2, row3)):
+        win = x
+    if all(i == ("X") for i in (row1[0], row2[0], row3[0)):
+        win = x
+    if all(i == ("X") for i in (row1[1], row2[1], row3[1)):
+        win = x
+    if all(i == ("X") for i in (row1[2], row2[2], row3[2])):
+        win = x
+    elif row1[0] == "X" and row2[1] == "X" and row3[2] == "X":
+        win = x
+    elif row3[0] == "X" and row2[1] == "X" and row1[2] == "X":
+        win = x
+    elif any(i == ("O", "O", "O") for i in (row1, row2, row3)):
+        win = o
+    elif row1[0] == "O" and row2[1] == "O" and row3[2] == "O":
+        win = x
+    elif row3[0] == "O" and row2[1] == "O" and row1[2] == "O":
+        win = x
+    elif row1 != (
         
+turn = 0
 
+while win = null:
+    if turn == 0:
+        Display()
+        l = int(input ("Input location of X"))
+        FindRow(l, "X")
+        turn += 1 
+
+    if turn == 1:
+        Display()
+        l = int(input ("Input location of O"))
+        FindRow(l, "O")
+        turn += 1
+
+    if turn > 1:
+        turn = 0
+        
