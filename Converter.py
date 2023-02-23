d949c94dee09325065750c8a80ac4b8427feeed9
Alex <99271006+Dragonbough@users.noreply.github.com>
2023-02-23 14:45:50 +0000

    converter

---
+++ Converter.py
@@ -0,0 +1,15 @@
+import math
+
+SIUnitsL = ("1. Meters", "2. Kilometers", "3. Centimeters", "4. Millimeters",\
+"5. Micrometers", "6. Nanometers", "7. Miles", "8. Yards", "9. Feet", "10. Inches")
+
+SIUnitsT = ("1. Celcius", "2. Fahrenheit", "3. Kelvin")
+
+SIUnitsW = ("1. Kilograms", "2. Grams", "3. Milligrams"
+
+print ("Measurement Converter")
+print ("What measurements would you like to convert?:")
+print ("What do you want to convert?:")
+print ("Lengths")
+print ("Temperatures")
+
