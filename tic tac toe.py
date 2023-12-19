c0463f06fdf819f4c62e6817c79d3710e94caf7c
dragonbough <fiveteen81@gmail.com>
2023-12-19 01:09:02 +0000

    cleaned stuff up

--- tic tac toe.py
+++ tic tac toe.py
@@ -1,9 +1,11 @@
 from operator import indexOf
 
 def initBoard():
-    print (board[0], board[1], board[2])
-    print (board[3], board[4], board[5])
-    print (board[6], board[7], board[8])
+    print (board[0], "|", board[1], "|", board[2])
+    print ("----------")
+    print (board[3], "|", board[4], "|", board[5])
+    print ("----------")
+    print (board[6], "|", board[7], "|", board[8])
 
 def checkBoard():
     #for X
