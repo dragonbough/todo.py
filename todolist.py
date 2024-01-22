e50d7dc8102a7b16af16ae684b12c10dc7586467
Alex <99271006+Dragonbough@users.noreply.github.com>
2024-01-22 15:01:57 +0000

    add foundations for todolist

--- todolist.py
+++ todolist.py
@@ -1,1 +1,38 @@
+todoListDict = {}
+
+def DisplayList(array):
+    for i in array:
+        print (str(array.index(i) + 1) + ". " + i)
+
+def CreateList():
+    todoList = []
+    listName = input ("What would you like to call your todo list? ")
+    endAppend = False
+    while endAppend == False:
+        task = input ("Add a task to your todo list; ENTER to stop:  ")
+        if len(task) > 0:
+            todoList.append(task)
+            DisplayList(todoList)
+        else:
+            endAppend = True 
+    todoListDict.update({listName:todoList})
+
+def ViewList():
+    
+    
+def EditList(listName):
+    
+
+print ("Welcome to PyList")
+if len(todoListDict) == 0:
+    CreateList()
+print (*todoListDict.keys())
+choice = input ("View, Edit or ")
+listName = ""
+while listName not in todoListDict.keys():
+    listName = input ("Which todo list would you like to view?")
+DisplayList(listName)
+
+
+
 
