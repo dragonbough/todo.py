3edc830fa924d6793f50b37e7cd8e5feadc6e3cc
Alex <99271006+Dragonbough@users.noreply.github.com>
2024-01-31 10:42:42 +0000

    started fde stuff

--- fdesim.py
+++ fdesim.py
@@ -1,1 +1,15 @@
+programs = {"add":[], "subtract":[]}
+RAM = {} 
 
+print (*programs.keys())
+program = input ("What program would you like to run?")
+RAM.update(program:programs[program]) 
+programcounter = RAM[program].index([0])
+MAR = programcounter
+addressbus = MAR
+databus = RAM[program][addressbus]
+MDR = databus 
+instructionregister = MDR 
+programcounter += 1
+
+    
